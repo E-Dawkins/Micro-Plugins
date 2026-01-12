7c8787640e5be19de671df0e1d4e8ac071c66b0f
E-Dawkins <ethandawkins@hotmail.co.uk>
2026-01-12 16:15:18 +1100

    Added weight unit conversion + unified printing

--- Unit_Converter/Plugins/MicroUnitConverter/Source/MicroUnitConverter/Private/MicroUnitConverterBPLibrary.cpp
+++ Unit_Converter/Plugins/MicroUnitConverter/Source/MicroUnitConverter/Private/MicroUnitConverterBPLibrary.cpp
@@ -15,26 +15,47 @@ double UMicroUnitConverterBPLibrary::ConvertUnit_Distance(double InValue, EDista
 	return InValue * (InRatio / OutRatio);
 }
 
-void UMicroUnitConverterBPLibrary::PrintConversion_Distance(double InValue, EDistanceUnit InUnit, EDistanceUnit OutUnit, int32 MinDecimalPlaces, int32 MaxDecimalPlaces, bool bPrintToScreen, bool bPrintToLog)
+void UMicroUnitConverterBPLibrary::PrintConversion_Distance(double InValue, EDistanceUnit InUnit, EDistanceUnit OutUnit, FPrintOptions PrintOptions)
+{
+	double ConvertedValue = ConvertUnit_Distance(InValue, InUnit, OutUnit);
+	FString OutUnitSuffix = DistanceSuffix[OutUnit];
+
+	PrintImpl(ConvertedValue, OutUnitSuffix, PrintOptions);
+}
+
+double UMicroUnitConverterBPLibrary::ConvertUnit_Weight(double InValue, EWeightUnit InUnit, EWeightUnit OutUnit)
+{
+	double InRatio = WeightRatios[InUnit];
+	double OutRatio = WeightRatios[OutUnit];
+
+	return InValue * (InRatio / OutRatio);
+}
+
+void UMicroUnitConverterBPLibrary::PrintConversion_Weight(double InValue, EWeightUnit InUnit, EWeightUnit OutUnit, FPrintOptions PrintOptions)
+{
+	double ConvertedValue = ConvertUnit_Weight(InValue, InUnit, OutUnit);
+	FString OutUnitSuffix = WeightSuffix[OutUnit];
+
+	PrintImpl(ConvertedValue, OutUnitSuffix, PrintOptions);
+}
+
+void UMicroUnitConverterBPLibrary::PrintImpl(double ConvertedValue, FString UnitSuffix, FPrintOptions PrintOptions)
 {
 #if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
 	FNumberFormattingOptions Opts = {};
-	Opts.MinimumFractionalDigits = MinDecimalPlaces;
-	Opts.MaximumFractionalDigits = MaxDecimalPlaces;
+	Opts.MinimumFractionalDigits = PrintOptions.MinDecimalPlaces;
+	Opts.MaximumFractionalDigits = PrintOptions.MaxDecimalPlaces;
 
-	double ConvertedValue = ConvertUnit_Distance(InValue, InUnit, OutUnit);
 	FText OutTextValue = FText::AsNumber(ConvertedValue, &Opts);
 
-	FString OutUnitSuffix = DistanceSuffix[OutUnit];
-
-	if (bPrintToScreen && GEngine)
+	if (PrintOptions.bPrintToScreen && GEngine)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Cyan, FString::Format(TEXT("{0}{1}"), { OutTextValue.ToString(), OutUnitSuffix }));
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Cyan, FString::Format(TEXT("{0}{1}"), { OutTextValue.ToString(), UnitSuffix }));
 	}
 
-	if (bPrintToLog)
+	if (PrintOptions.bPrintToLog)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("%s%s"), *OutTextValue.ToString(), *OutUnitSuffix);
+		UE_LOG(LogTemp, Warning, TEXT("%s%s"), *OutTextValue.ToString(), *UnitSuffix);
 	}
 #endif
 }
--- Unit_Converter/Plugins/MicroUnitConverter/Source/MicroUnitConverter/Public/MicroUnitConverterBPLibrary.h
+++ Unit_Converter/Plugins/MicroUnitConverter/Source/MicroUnitConverter/Public/MicroUnitConverterBPLibrary.h
@@ -3,6 +3,24 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "MicroUnitConverterBPLibrary.generated.h"
 
+USTRUCT(BlueprintType)
+struct FPrintOptions
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 MinDecimalPlaces = 0;
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 MaxDecimalPlaces = 6;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bPrintToScreen = true;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bPrintToLog = true;
+};
+
 UENUM(BlueprintType)
 enum class EDistanceUnit : uint8
 {
@@ -18,6 +36,18 @@ enum class EDistanceUnit : uint8
 	EDU_Lightyears		UMETA(DisplayName = "Lightyears"),
 };
 
+UENUM(BlueprintType)
+enum class EWeightUnit : uint8
+{
+	EWU_Micrograms		UMETA(DisplayName = "Micrograms"),
+	EWU_Milligrams		UMETA(DisplayName = "Milligrams"),
+	EWU_Grams			UMETA(DisplayName = "Grams"),
+	EWU_Kilograms		UMETA(DisplayName = "Kilograms"),
+	EWU_MetricTons		UMETA(DisplayName = "Metric Tons"),
+	EWU_Ounces			UMETA(DisplayName = "Ounces"),
+	EWU_Pounds			UMETA(DisplayName = "Pounds"),
+	EWU_Stones			UMETA(DisplayName = "Stones"),
+};
 
 UCLASS()
 class UMicroUnitConverterBPLibrary : public UBlueprintFunctionLibrary
@@ -29,8 +59,19 @@ class UMicroUnitConverterBPLibrary : public UBlueprintFunctionLibrary
 	static double ConvertUnit_Distance(double InValue, EDistanceUnit InUnit, EDistanceUnit OutUnit);
 
 	// Prints the converted distance value to screen / output log
-	UFUNCTION(BlueprintCallable, Category = "Micro Unit Converter", meta = (DisplayName = "Print Conversion - Distance", DevelopmentOnly, AdvancedDisplay = 3))
-	static void PrintConversion_Distance(double InValue, EDistanceUnit InUnit, EDistanceUnit OutUnit, int32 MinDecimalPlaces = 0, int32 MaxDecimalPlaces = 6, bool bPrintToScreen = true, bool bPrintToLog = true);
+	UFUNCTION(BlueprintCallable, Category = "Micro Unit Converter", meta = (DisplayName = "Print Conversion - Distance", DevelopmentOnly))
+	static void PrintConversion_Distance(double InValue, EDistanceUnit InUnit, EDistanceUnit OutUnit, FPrintOptions PrintOptions);
+
+	// Converts value from one weight unit to another using the appropriate ratio
+	UFUNCTION(BlueprintPure, Category = "Micro Unit Converter", meta = (DisplayName = "Convert Unit - Weight"))
+	static double ConvertUnit_Weight(double InValue, EWeightUnit InUnit, EWeightUnit OutUnit);
+
+	// Prints the converted weight value to screen / output log
+	UFUNCTION(BlueprintCallable, Category = "Micro Unit Converter", meta = (DisplayName = "Print Conversion - Weight", DevelopmentOnly))
+	static void PrintConversion_Weight(double InValue, EWeightUnit InUnit, EWeightUnit OutUnit, FPrintOptions PrintOptions);
+
+private:
+	static void PrintImpl(double ConvertedValue, FString UnitSuffix, FPrintOptions PrintOptions);
 
 private:
 	// Output = Input * (RatioFrom/RatioTo)
@@ -62,6 +103,31 @@ private:
 		{ EDistanceUnit::EDU_Yards,			"yd" },
 		{ EDistanceUnit::EDU_Miles,			"mi" },
 		{ EDistanceUnit::EDU_Lightyears,	"ly" },
+	};
+
+	// These ratios are measured in grams, i.e. 1mg = 0.001g
+	inline static const TMap<EWeightUnit, double> WeightRatios =
+	{
+		{ EWeightUnit::EWU_Micrograms,		0.000001 },
+		{ EWeightUnit::EWU_Milligrams,		0.001 },
+		{ EWeightUnit::EWU_Grams,			1.0 },
+		{ EWeightUnit::EWU_Kilograms,		1000.0 },
+		{ EWeightUnit::EWU_MetricTons,		1000000.0 },
+		{ EWeightUnit::EWU_Ounces,			28.3495231 },
+		{ EWeightUnit::EWU_Pounds,			453.59237 },
+		{ EWeightUnit::EWU_Stones,			6350.29317 },
+	};
+
+	inline static const TMap<EWeightUnit, FString> WeightSuffix =
+	{
+		{ EWeightUnit::EWU_Micrograms,		"µg" },
+		{ EWeightUnit::EWU_Milligrams,		"mg" },
+		{ EWeightUnit::EWU_Grams,			"g" },
+		{ EWeightUnit::EWU_Kilograms,		"kg" },
+		{ EWeightUnit::EWU_MetricTons,		"t" },
+		{ EWeightUnit::EWU_Ounces,			"oz" },
+		{ EWeightUnit::EWU_Pounds,			"lb" },
+		{ EWeightUnit::EWU_Stones,			"st" },
 	};
 	
 };
